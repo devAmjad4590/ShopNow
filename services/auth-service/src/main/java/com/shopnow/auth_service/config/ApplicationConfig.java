6c78c2fca32f68b9f433c6127a573e65bcdb464a
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2025-11-22 11:51:14 +0800

    Created App and Security config

---
+++ services/auth-service/src/main/java/com/shopnow/auth_service/config/ApplicationConfig.java
@@ -0,0 +1,28 @@
+package com.shopnow.auth_service.config;
+
+import com.shopnow.auth_service.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("This user does not exist"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ services/auth-service/src/main/java/com/shopnow/auth_service/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.shopnow.auth_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// needed cuz spring security protect routes by default
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+http.
+authorizeHttpRequests(auth -> auth
+        .requestMatchers("/api/v1/auth/**").permitAll() // public routes
+        .anyRequest().authenticated());
+        return http.build();
+    }
+}
