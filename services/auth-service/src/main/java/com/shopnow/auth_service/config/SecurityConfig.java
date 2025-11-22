129d08b84393190a5097c44a20868a82f0dc0909
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2025-11-22 12:11:51 +0800

    Added SecurityConfig, created public endpoints and disabled csrf

--- services/auth-service/src/main/java/com/shopnow/auth_service/config/SecurityConfig.java
+++ services/auth-service/src/main/java/com/shopnow/auth_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 // needed cuz spring security protect routes by default
@@ -14,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 http.
+        csrf(AbstractHttpConfigurer::disable). // to allow public post req to get through
 authorizeHttpRequests(auth -> auth
         .requestMatchers("/api/v1/auth/**").permitAll() // public routes
         .anyRequest().authenticated());
