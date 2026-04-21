ba57aa65b5abd8307686ea67293d19c940f6cd08
devAmjad4590 <developeramjad4590@gmail.com>
2026-04-21 16:26:12 +0300

    fix(user-service): use primitive boolean for isDefault in AddressRequest

--- services/user-service/src/main/java/com/shopnow/user_service/address/AddressRequest.java
+++ services/user-service/src/main/java/com/shopnow/user_service/address/AddressRequest.java
@@ -28,5 +28,5 @@ public class AddressRequest {
     @NotBlank(message = "Postal code is required")
     private String postalCode;
 
-    private Boolean isDefault;
+    private boolean isDefault;
 }
