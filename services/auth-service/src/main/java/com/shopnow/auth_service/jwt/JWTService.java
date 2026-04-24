f797b026b98e5611d88e6f0e8561a916a43d57ba
devAmjad4590 <developeramjad4590@gmail.com>
2026-04-24 15:05:53 +0300

    fix(auth-service): add missing io.jsonwebtoken.io.Decoders import

--- services/auth-service/src/main/java/com/shopnow/auth_service/jwt/JWTService.java
+++ services/auth-service/src/main/java/com/shopnow/auth_service/jwt/JWTService.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
